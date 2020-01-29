e221385ea6fc931f4d7bf11e6690700fe39d4b57
chhabrabhishek <abhishekchhabra107@gmail.com>
2020-01-29 09:31:58 +0530

    Add env prod support, baby!

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDs84uIzELHbFgxvi2aO8hDWXXVY1v0dCo",
+    authDomain: "happy-birthday-beautiful.firebaseapp.com",
+    databaseURL: "https://happy-birthday-beautiful.firebaseio.com",
+    projectId: "happy-birthday-beautiful",
+    storageBucket: "happy-birthday-beautiful.appspot.com",
+    messagingSenderId: "1093121212694",
+    appId: "1:1093121212694:web:8d24b45e2fbe6022fed037",
+    measurementId: "G-VFX0J3VTRT"
+  }
 };
